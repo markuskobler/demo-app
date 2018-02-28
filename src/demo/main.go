0f09007e35190f65decb13ff067af97b6b5ab407
mkobler <mkobler@cargurus.com>
2018-02-27 20:13:34 -0500

    Initial server commit

---
+++ src/demo/main.go
@@ -0,0 +1,52 @@
+//go:generate go-bindata -pkg $GOPACKAGE -o assets.go -prefix assets assets/
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+var (
+	commit = "DEV"
+
+	addr = ":8888"
+	name = "demo1"
+)
+
+func main() {
+	flag.StringVar(&addr, "addr", addr, "Address to listen on (default ':8888')")
+	flag.StringVar(&name, "name", name, "Server name")
+	flag.Parse()
+
+	http.HandleFunc("/", Index)
+
+	fmt.Printf("%s (%s)\n", name, commit)
+	fmt.Printf("Listening on %s\n", addr)
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+var index *template.Template = template.Must(template.New("index").Parse(string(MustAsset("index.html"))))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%s %s\n", r.Method, r.URL)
+	w.Header().Set("Content-Type", "text/html; charset=UTF-8")
+	w.Header().Set("Set-Cookie", "JSESSIONID="+name)
+	if refresh := r.FormValue("r"); refresh != "" {
+		w.Header().Set("Refresh", refresh)
+	}
+	w.WriteHeader(http.StatusOK)
+
+	index.Execute(w, struct {
+		Name    string
+		Headers http.Header
+	}{
+		Name:    name,
+		Headers: r.Header,
+	})
+}
